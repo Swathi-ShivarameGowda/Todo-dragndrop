0964619f7f03ebbe1b9a45ad7ad25469de781294
Swathi-ShivarameGowda <swathi.shivaramegowda@gmail.com>
2021-07-29 14:10:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
 
 function App() {
+    const [inputValue, setInputValue] = useState()
+    const [list, setList] = useState([]);
+    const [idValue, setIdvalue]=useState(0)
+    
+    const addItem = (item) => {
+        if (item) {
+            setIdvalue(idValue + 1);
+            const newList = { item, id: idValue }
+            setList([newList, ...list]);
+            setInputValue("")
+        }
+    }
+
+    
+
+    const removeItem = (item) => {
+        let itemArray = Array.from(list);
+        itemArray.splice(item, 1);
+        setList(itemArray)
+   }
+
+    function handleOnDragEnd(result) {
+        if (!result.destination) return;
+
+        const items = Array.from(list);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+
+        setList(items);
+    }
+
+    const itemCheckHandler = (i) => {
+        setList(
+            list.map(
+                (j, k) =>
+                    k === i
+                        ? {
+                            ...j,
+                            itemClicked: !j.itemClicked
+                        }
+                        : j
+            )
+        )
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <div className="App">
+              <header className="App-header">
+                  <h1>Todo List</h1>
+
+                  <div className="Add">
+                      <input value={inputValue} onChange={e => setInputValue(e.target.value)} />
+                      <button onClick={() => addItem(inputValue)}>Add</button>
+                  </div>
+                  
+                  <DragDropContext onDragEnd={handleOnDragEnd}>
+                      <Droppable droppableId="list">
+                          {(provided) => (
+
+                              <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
+
+                                  {list.map(({item, id }, index) => {
+                                      
+                                  return (
+                                      <Draggable key={id} draggableId={id.toString()} index={index}>
+                                          {(provided) => (
+                                              <div className="item-ul">
+
+                                              <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} key={id}>
+                                                
+
+                                                  <div className="characters-thumb">
+
+                                                      {item}
+
+                                                  </div>
+                                                  <button onClick={() => removeItem(index)}>Remove</button>
+
+
+                                                  </li>
+                                              </div>
+
+                                          )}
+
+                                      </Draggable>
+                                   );     
+
+                                      })}
+                                      {provided.placeholder}
+                              </ul>
+                                   )}
+                      </Droppable>
+                      </DragDropContext>
+                      
+              </header>
+          </div>
+      </>
   );
 }
 
